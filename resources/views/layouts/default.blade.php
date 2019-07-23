9454af1256c3e63c7d89d33b9937614ac5b9c614
zou <979768992@qq.com>
2019-07-23 04:52:52 +0000

    前端缓存及路由问题” “

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title')-NEW START</title>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
-    @yield('content')
+    @include('layouts._header')
+    <div class="container">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </body>
 </html>
