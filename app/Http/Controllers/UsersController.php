100a7b108d5527400974f0278ac725f0557754ee
zou <979768992@qq.com>
2019-07-23 10:08:22 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
@@ -11,4 +12,10 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
 }
