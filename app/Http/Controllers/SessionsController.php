8667c564f662ef0277efe4f60188eaddb7f301d0
zou <979768992@qq.com>
2019-07-24 04:28:14 +0000

    登录记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -21,7 +21,7 @@ class SessionsController extends Controller
            'password' => 'required'
        ]);
 
-        if(Auth::attempt($credentials))
+        if(Auth::attempt($credentials, $request->has('remember')))
         {
             session()->flash('success', '欢迎回来！');
             return redirect()->route('users.show', [Auth::user()]);
@@ -31,8 +31,6 @@ class SessionsController extends Controller
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
-
-       return;
     }
 
     public function destroy()
