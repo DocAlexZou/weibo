a54ea36ddc6b27884a3d4a050d030d793800f439
zou <979768992@qq.com>
2019-07-29 04:32:26 +0000

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -30,9 +30,19 @@ class SessionsController extends Controller
 
         if(Auth::attempt($credentials, $request->has('remember')))
         {
-            session()->flash('success', '欢迎回来！');
-            $fallback = route('users.show', Auth::user());
-            return redirect()->intended($fallback);
+            if(Auth::user()->activated)
+            {
+                session()->flash('success', '欢迎回来！');
+                $fallback = route('users.show', Auth::user());
+                return redirect()->intended($fallback);
+            }
+            else
+            {
+                Auth::logout();
+                session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+                return redirect()->back()->withInput();
+            }
+
         }
         else
         {
